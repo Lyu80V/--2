703c1205911be184993c4cd369c0eb0962a56124
Lyu80V <7222707@gmail.com>
2024-11-14 15:21:48 +0300

    Add files via upload

---
+++ Базовые структуры данных.py
@@ -0,0 +1,18 @@
+#"1st program"
+print(9**0.5*5)
+
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#print(1000!=1000.1)
+
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#"4th program"
+'''a = '123.456'
+print(a[4])'''
+b = float('123.456')
+c = b*10
+print(int(c)%10)
